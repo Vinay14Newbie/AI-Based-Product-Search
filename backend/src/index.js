9fc37e9dadf0188e5afd8aa07c95f57dbdd1ce1d
Vinay14Newbie <wjust1984@gmail.com>
2026-02-28 20:10:07 +0530

    Small fix in index.js file

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,8 +16,6 @@ app.get("/", (req, res) => {
   });
 });
 
-const PORT = PORT || 5000;
-
-app.listen(PORT, () => {
+app.listen(PORT || 5000, () => {
   console.log(`Server running on port ${PORT}`);
 });
